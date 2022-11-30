e9545d8bfb351aa09a8d9208d95379ff4ccacead
O-GODWIN <kosokoink@gmail.com>
2022-11-30 05:44:20 +0100

    commit

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-for i in range(97, 123):
-    j = chr(i)
-    if j != 'q' and j != 'e':
-        print("{}".format(j), end="")
+for letter in range(97, 123):
+    if chr(letter) is not 'q' and chr(letter) is not 'e':
+        print("{}".format(chr(letter)), end="")
