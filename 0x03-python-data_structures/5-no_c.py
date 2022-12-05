638c54f1f90be95f8586ede8518b3bfd673d2d3d
O-GODWIN <kosokoink@gmail.com>
2022-12-05 22:17:40 +0100

    fix

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,13 +1,6 @@
 #!/usr/bin/python3
-# 6-print_matrix_integer.py
 
-
-def print_matrix_integer(matrix=[[]]):
-    """Print a matrix of integers."""
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-                print("{:d}".format(matrix[i][j]), end="")
-                if j != (len(matrix[i]) - 1):
-                    print(" ", end="")
-
-        print("")
+def no_c(my_string):
+    """Remove all characters c and C from a string."""
+    copy = [x for x in my_string if x != 'c' and x != 'C']
+    return ("".join(copy))
